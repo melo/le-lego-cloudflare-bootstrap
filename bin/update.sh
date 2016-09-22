b02b5b7c5ef6b3d91688d43c1636f2151a8a0ec5
Pedro Melo <melo@simplicidade.org>
2016-09-22 04:28:45 +0100

    Added first draft of update.sh script and friends
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ bin/update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Create or renew certificates for a set of domains
+#
+
+## location of certificates directories
+certs_dir="./certificates_specs"
+
+
+## Die on errors and load our utils
+set -e
+. "./lib/utils.sh"
+
+
+## Which certificates to update?
+certs="$@"
+git_commit_globals
+if [ -z "$certs" ] ; then
+  ls -1 "$certs_dir/"*.sh | while read cert_file ; do
+    update_single_cert "$cert_file"
+  done
+else
+  for cert_file in "$@" ; do
+    update_single_cert "$cert_file"
+  done
+fi
+
+
+
---
+++ lib/update_single.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+#
+# Create or renew certificates for a single domain
+#
+
+## Die on errors and load our utils
+set -e
+. "./lib/utils.sh"
+
+
+## Inputs
+cert_file="$1"
+if [ -z "$cert_file" ] ; then
+  echo "Usage: ./bin/update_single.sh certificate_spec_file"
+  exit 2
+fi
+
+cert_name=`basename "$cert_file"`
+echo "*** Updating '$cert_name' certificate"
+printf "*** at "
+date
+
+
+## Check to see if we have a valid domain to update
+
+if [ -e "$cert_file" ] ; then
+  echo "... reading certificate '$cert_name' spec file"
+  . "$cert_file"
+fi
+
+exit_if_disabled "FATAL: certificate '$cert_name' disabled on the local $cert_file"
+
+if [ -z "$DOMAIN" ] ; then
+  fatal "no domain to update on certificate '$cert_name'" "local $cert_file lacks a DOMAIN configuration"
+fi
+
+
+## Check for other required settings
+for cfvar in "CLOUDFLARE_EMAIL" "CLOUDFLARE_API_KEY" "EMAIL" "KEY_TYPE"; do
+  if [ -z "${!cfvar}" ] ; then
+    fatal "missing $cfvar definition" "add it to the global or local $cert_file"
+  fi
+done
+export CLOUDFLARE_EMAIL CLOUDFLARE_API_KEY
+
+
+## Check for lego
+printf "... using "
+lego --version
+
+
+## Production or Staging?
+server=""
+if [ "$ENVIRONMENT" = "production" ] ; then
+  echo "... using **Production** environment"
+else
+  server="--server https://acme-staging.api.letsencrypt.org/directory"
+  echo "... using Staging environment"
+fi
+
+
+## Create or renew?
+
+if [ -e "certificates/$DOMAIN.key" -a -e "certificates/$DOMAIN.crt" -a -e "certificates/$DOMAIN.json" ] ; then
+  ## Renew the certificate!
+  echo "... renewing the certificate for domain '$DOMAIN'"
+  set -x
+  echo lego \
+        --path "." \
+        --email="$EMAIL" \
+        --domains="$DOMAIN" \
+        --dns cloudflare \
+        --key-type "$KEY_TYPE" \
+        --accept-tos \
+        $server renew
+  set +x
+else    
+  ## Create the certificate!
+  echo "... creating certificate for domain '$DOMAIN'"
+  set -x
+  echo lego \
+      --path "." \
+      --email="$EMAIL" \
+      --domains="$DOMAIN" \
+      --dns cloudflare \
+      --key-type "$KEY_TYPE" \
+      --accept-tos \
+      $server run
+  set +x
+fi
+
+if [ ! -e "certificates/$DOMAIN.key" -o ! -e "certificates/$DOMAIN.crt" -o ! -e "certificates/$DOMAIN.json" ] ; then
+  fatal "failed to create certificate for '$DOMAIN'"
+fi
+
+git_commit "Updated certificate $cert for domain $DOMAIN" "$cert_file" "./certificates/$DOMAIN.*"
